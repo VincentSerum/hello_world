e84c2b1c6b1b16806060049f764a4dc13eea2636
legen <legen@Vincent-Surfacebook>
2018-12-08 18:42:11 +0900

    just add a line to print

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,5 +4,6 @@ public class helloworld {
     public static void main(String[] args) {
     	System.out.println("Hello world");
     	System.out.println("vincent ace Serum");
-    }
+    	System.out.println("void");
+    	}
 }
